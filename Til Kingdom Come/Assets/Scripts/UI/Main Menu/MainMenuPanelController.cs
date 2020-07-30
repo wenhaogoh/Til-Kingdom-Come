f0a5cf3548fb28c486de6f541858550580542409
EkChinHui <57344608+EkChinHui@users.noreply.github.com>
2020-07-30 22:22:17 +0800

    Add selection menu elements

--- Til Kingdom Come/Assets/Scripts/UI/Main Menu/MainMenuPanelController.cs
+++ Til Kingdom Come/Assets/Scripts/UI/Main Menu/MainMenuPanelController.cs
@@ -1,30 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MainMenuPanelController : MonoBehaviour
+namespace UI.Main_Menu
 {
-    public GameObject mainMenuButtons;
-    public GameObject playMenuButtons;
-    public GameObject settingsMenuButtons;
-    private Vector3 zeroVector = new Vector3(0, 0, 0);
-    private Vector3 oneVector = new Vector3(1, 1, 1);
-    public void LaunchMainMenu()
+    public class MainMenuPanelController : MonoBehaviour
     {
-        mainMenuButtons.transform.localScale = oneVector;
-        playMenuButtons.transform.localScale = zeroVector;
-        settingsMenuButtons.transform.localScale = zeroVector;
-    }
-    public void LaunchPlayMenu()
-    {
-        playMenuButtons.transform.localScale = oneVector;
-        mainMenuButtons.transform.localScale = zeroVector;
-        settingsMenuButtons.transform.localScale = zeroVector;
-    }
-    public void LaunchSettingsMenu()
-    {
-        settingsMenuButtons.transform.localScale = oneVector;
-        playMenuButtons.transform.localScale = zeroVector;
-        mainMenuButtons.transform.localScale = zeroVector;
+        public GameObject mainMenuButtons;
+        public GameObject playMenuButtons;
+        public GameObject settingsMenuButtons;
+        private Vector3 zeroVector = new Vector3(0, 0, 0);
+        private Vector3 oneVector = new Vector3(1, 1, 1);
+        public void LaunchMainMenu()
+        {
+            mainMenuButtons.transform.localScale = oneVector;
+            playMenuButtons.transform.localScale = zeroVector;
+            settingsMenuButtons.transform.localScale = zeroVector;
+        }
+        public void LaunchPlayMenu()
+        {
+            playMenuButtons.transform.localScale = oneVector;
+            mainMenuButtons.transform.localScale = zeroVector;
+            settingsMenuButtons.transform.localScale = zeroVector;
+        }
+        public void LaunchSettingsMenu()
+        {
+            settingsMenuButtons.transform.localScale = oneVector;
+            playMenuButtons.transform.localScale = zeroVector;
+            mainMenuButtons.transform.localScale = zeroVector;
+        }
     }
 }
